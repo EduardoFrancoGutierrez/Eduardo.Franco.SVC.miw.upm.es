cc1f4507f28d2fe4e64a61a4dedc83fe98706f94
eduardofranco <eduardofranco@192.168.1.105>
2016-09-28 22:25:01 +0200

    Arreglo del error del metodo m1 de la c11 y cambio del pom a la version 1.0.1

--- src/main/java/es/upm/miw/iwvg/clase/C11.java
+++ src/main/java/es/upm/miw/iwvg/clase/C11.java
@@ -3,7 +3,8 @@ package es.upm.miw.iwvg.clase;
 public class C11 {
     
     public String  m1 (){
-        return "m1";
+        String retorno="m1";
+        return retorno;
     }
     
     public String  m2 (){
